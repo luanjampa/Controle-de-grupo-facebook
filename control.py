7bb4130bdd7df4e048968164b374755c34c48823
luanjampa <luansouza.jampa@gmail.com>
2013-10-16 11:41:36 -0300

    Update control.py

--- control.py
+++ control.py
@@ -3,7 +3,7 @@ import json
 #copie o formato no exemplo e pegue o access_token
 #em https://developers.facebook.com/tools/explorer
 proibido = 'pode'
-url = 'https://graph.facebook.com/367875099923704/feed?access_token=CAACEdEose0cBAFZCKxA538zDHbLPiQhTznexTZBxhf9oo1pRmiyctBuCzFpanBb8FjVX8MHym9Dh4jEVOKhGR75ZBqf1gduznBcrI6QtHKZC4h7rEmge5cQjStc67UN6YJHklvgeZCZBcwd6isSZCyFXiQR7nlKYfw7dnWuaa4bQRPNxNXsZCiAdFqNlOrSfw8xoswQ3Ltrx5QZDZD'
+url = 'https://graph.facebook.com/367875099923704/feed?access_token=<token>'
 resp = urllib.request.urlopen(url).read()
 data = json.loads(resp.decode('utf-8'))
 postagem = data['data'][0]
